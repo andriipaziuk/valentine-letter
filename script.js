65164972e221043e4fce7ded5bd26ebb3d43156c
Andrii Paziuk <62886373+andriipaziuk@users.noreply.github.com>
2024-02-12 13:42:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.querySelector(".container");
     const card = document.querySelector(".card");
+    const body = document.querySelector("body");
 
     container.addEventListener("mouseenter", () => {
-        card.style.transition = "top 0.5s";
-        card.style.top = "-90px";
-    });
-
-     container.addEventListener("click", () => {
         card.style.transition = "top 0.5s";
         card.style.top = "-90px";
     });
@@ -16,4 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
         card.style.transition = "top 0.5s";
         card.style.top = "0";
     });
+
+    container.addEventListener("click", () => {
+        card.style.transition = "top 0.5s";
+        card.style.top = "-90px";
+    }, );
+
+    body.addEventListener("click", () => {
+        card.style.transition = "top 0.5s";
+        card.style.top = "0";
+    }, true);
 });
